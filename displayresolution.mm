1583ed340cf0b5806ff96e169123a0c4f7bfbfc6
Steven Rabulan <steven@stevenrabulan.com>
2024-07-23 10:57:31 -0500

    Add commands to increment resolutions up or down to the next resolution for easy keybinding

--- displayresolution.mm
+++ displayresolution.mm
@@ -9,9 +9,11 @@ COMPILE:
    (add to $PATH)
 USE:
     displayresolution [shows current display dimensions]
-  	displayresolution list
-	displayresolution 1728x1080 1920x1080 1920x1080
- 	 displayresolution skip 1920x1080 skip
+    displayresolution list
+    displayresolution 1728x1080 1920x1080 1920x1080
+    displayresolution skip 1920x1080 skip
+    displayresolution up
+    displayresolution down
 */
 
 #include <ApplicationServices/ApplicationServices.h>
@@ -20,6 +22,7 @@ USE:
 #include <set>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 
 bool displayresolutionSwitchToMode(CGDirectDisplayID display, CGDisplayModeRef mode);
 
@@ -31,8 +34,39 @@ std::pair<int, int> parseResolution(const std::string& res) {
     return {width, height};
 }
 
+std::vector<std::pair<int, int>> getSortedResolutions(CGDirectDisplayID display, CFDictionaryRef options) {
+    CFArrayRef allModes = CGDisplayCopyAllDisplayModes(display, options);
+    std::vector<std::pair<int, int>> resolutions;
+
+    if (allModes != NULL) {
+        for (CFIndex i = 0; i < CFArrayGetCount(allModes); i++) {
+            CGDisplayModeRef mode = (CGDisplayModeRef)CFArrayGetValueAtIndex(allModes, i);
+            int width = (int)CGDisplayModeGetWidth(mode);
+            int height = (int)CGDisplayModeGetHeight(mode);
+            resolutions.push_back({width, height});
+        }
+        CFRelease(allModes);
+        std::sort(resolutions.begin(), resolutions.end());
+        resolutions.erase(std::unique(resolutions.begin(), resolutions.end()), resolutions.end());
+    }
+
+    return resolutions;
+}
+
+std::pair<int, int> getNextResolution(const std::vector<std::pair<int, int>>& resolutions, std::pair<int, int> current, bool up) {
+    auto it = std::find(resolutions.begin(), resolutions.end(), current);
+    if (it != resolutions.end()) {
+        if (up) {
+            if (it + 1 != resolutions.end()) return *(it + 1);
+        } else {
+            if (it != resolutions.begin()) return *(it - 1);
+        }
+    }
+    return current; // Return current if next resolution is not found
+}
+
 int main(int argc, const char * argv[]) {
-    if (argc == 2 && strcmp(argv[1], "list") == 0) {
+    if (argc == 2 && (strcmp(argv[1], "list") == 0 || strcmp(argv[1], "up") == 0 || strcmp(argv[1], "down") == 0)) {
         uint32_t displayCount;
         CGGetActiveDisplayList(0, NULL, &displayCount);
         std::vector<CGDirectDisplayID> displays(displayCount);
@@ -47,122 +81,41 @@ int main(int argc, const char * argv[]) {
             &kCFTypeDictionaryValueCallBacks
         );
 
-        for (uint32_t i = 0; i < displayCount; ++i) {
-            CFArrayRef allModes = CGDisplayCopyAllDisplayModes(displays[i], options);
-            if (allModes == NULL) {
-                fprintf(stderr, "ERROR: Unable to get display modes for display %u.\n", i);
-                CFRelease(options);
-                return -1;
+        if (strcmp(argv[1], "list") == 0) {
+            for (uint32_t i = 0; i < displayCount; ++i) {
+                CFArrayRef allModes = CGDisplayCopyAllDisplayModes(displays[i], options);
+                if (allModes == NULL) {
+                    fprintf(stderr, "ERROR: Unable to get display modes for display %u.\n", i);
+                    CFRelease(options);
+                    return -1;
+                }
+
+                std::set<std::pair<int, int>> uniqueResolutions;
+                for (CFIndex j = 0; j < CFArrayGetCount(allModes); j++) {
+                    CGDisplayModeRef mode = (CGDisplayModeRef)CFArrayGetValueAtIndex(allModes, j);
+                    int width = (int)CGDisplayModeGetWidth(mode);
+                    int height = (int)CGDisplayModeGetHeight(mode);
+                    uniqueResolutions.insert(std::make_pair(width, height));
+                }
+
+                printf("Display %u available resolutions:\n", i);
+                for (const auto& res : uniqueResolutions) {
+                    printf("%dx%d\n", res.first, res.second);
+                }
+
+                CFRelease(allModes);
             }
 
-            std::set<std::pair<int, int> > uniqueResolutions;
-            for (CFIndex j = 0; j < CFArrayGetCount(allModes); j++) {
-                CGDisplayModeRef mode = (CGDisplayModeRef)CFArrayGetValueAtIndex(allModes, j);
-                int width = (int)CGDisplayModeGetWidth(mode);
-                int height = (int)CGDisplayModeGetHeight(mode);
-                uniqueResolutions.insert(std::make_pair(width, height));
-            }
-
-            printf("Display %u available resolutions:\n", i);
-            for (const auto& res : uniqueResolutions) {
-                printf("%dx%d\n", res.first, res.second);
-            }
-
-            CFRelease(allModes);
-        }
-
-        CFRelease(options);
-        return 0;
-    }
-
-    if (argc == 1) {
-        uint32_t displayCount;
-        CGGetActiveDisplayList(0, NULL, &displayCount);
-        std::vector<CGDirectDisplayID> displays(displayCount);
-        CGGetActiveDisplayList(displayCount, displays.data(), &displayCount);
-
-        for (uint32_t i = 0; i < displayCount; ++i) {
-            CGDisplayModeRef currentMode = CGDisplayCopyDisplayMode(displays[i]);
-            if (currentMode != NULL) {
-                printf("Display %u current resolution: %zux%zu\n", i, CGDisplayModeGetWidth(currentMode), CGDisplayModeGetHeight(currentMode));
-                CGDisplayModeRelease(currentMode);
-            } else {
-                printf("Unable to get current display mode for display %u\n", i);
-            }
-        }
-        return 0;
-    }
-
-    uint32_t displayCount;
-    CGGetActiveDisplayList(0, NULL, &displayCount);
-    std::vector<CGDirectDisplayID> displays(displayCount);
-    CGGetActiveDisplayList(displayCount, displays.data(), &displayCount);
-
-    CFDictionaryRef options = CFDictionaryCreate(
-        kCFAllocatorDefault,
-        (const void *[]) { kCGDisplayShowDuplicateLowResolutionModes },
-        (const void *[]) { kCFBooleanTrue },
-        1,
-        &kCFTypeDictionaryKeyCallBacks,
-        &kCFTypeDictionaryValueCallBacks
-    );
-
-    for (int i = 1; i < argc && i - 1 < displayCount; ++i) {
-        std::string resolution = argv[i];
-        if (resolution == "skip") continue;
-
-        auto [width, height] = parseResolution(resolution);
-        if (width <= 0 || height <= 0) {
-            fprintf(stderr, "ERROR: Invalid resolution format: %s\n", argv[i]);
             CFRelease(options);
-            return -1;
+            return 0;
         }
 
-        CFArrayRef allModes = CGDisplayCopyAllDisplayModes(displays[i - 1], options);
-        if (allModes == NULL) {
-            fprintf(stderr, "ERROR: Unable to get display modes for display %u.\n", i - 1);
+        if (strcmp(argv[1], "up") == 0 || strcmp(argv[1], "down") == 0) {
+            bool up = (strcmp(argv[1], "up") == 0);
+
+            CGDirectDisplayID display = displays[0]; // Assuming display 0 for now
+            std::vector<std::pair<int, int>> resolutions = getSortedResolutions(display, options);
             CFRelease(options);
-            return -1;
-        }
 
-        CGDisplayModeRef switchMode = NULL;
-        for (CFIndex j = 0; j < CFArrayGetCount(allModes); j++) {
-            CGDisplayModeRef mode = (CGDisplayModeRef)CFArrayGetValueAtIndex(allModes, j);
-            if (CGDisplayModeGetWidth(mode) == width && CGDisplayModeGetHeight(mode) == height) {
-                switchMode = mode;
-                break;
-            }
-        }
-
-        if (switchMode == NULL) {
-            fprintf(stderr, "ERROR: No suitable mode found for resolution %dx%d on display %u.\n", width, height, i - 1);
-            CFRelease(allModes);
-            CFRelease(options);
-            return 1;
-        }
-
-        if (!displayresolutionSwitchToMode(displays[i - 1], switchMode)) {
-            fprintf(stderr, "ERROR: Failed to change resolution to %dx%d on display %u.\n", width, height, i - 1);
-            CFRelease(allModes);
-            CFRelease(options);
-            return 1;
-        }
-
-        printf("Successfully changed resolution to %dx%d on display %u.\n", width, height, i - 1);
-        CFRelease(allModes);
-    }
-
-    CFRelease(options);
-    return 0;
-}
-
-bool displayresolutionSwitchToMode(CGDirectDisplayID display, CGDisplayModeRef mode) {
-    CGDisplayConfigRef config;
-    if (CGBeginDisplayConfiguration(&config) == kCGErrorSuccess) {
-        CGConfigureDisplayWithDisplayMode(config, display, mode, NULL);
-        if (CGCompleteDisplayConfiguration(config, kCGConfigureForSession) == kCGErrorSuccess) {
-            return true;
-        }
-    }
-    return false;
-}
+            CGDisplayModeRef currentMode = CGDisplayCopyDisplayMode(display);
+            if (currentMode 
